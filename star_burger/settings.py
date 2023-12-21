6aed4852f18f16050f5f0c11b29bea0699145bb4
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-12-21 18:33:27 +0400

    switch to PostgreSQL DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -89,9 +89,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.str('DB_PORT'),
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
