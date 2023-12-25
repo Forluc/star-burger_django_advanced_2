6a203d66f1c9ecd7fcd3dcfd70acb1f1f069806f
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-12-25 22:52:01 +0400

    Disable interactive input in the deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -3,7 +3,7 @@
 git pull
 pip install -r requirements.txt
 npm ci --dev
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 python3 manage.py migrate
 
 echo -e "\033[32mSucceeded"
