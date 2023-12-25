69ad9ae0bf7963d46854b9459e4d3ad2814f3da0
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-12-25 22:34:38 +0400

    Make the deploy_star_burger.sh script notice errors

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
 git pull
 pip install -r requirements.txt
 npm ci --dev
 python3 manage.py collectstatic
 python3 manage.py migrate
-systemctl daemon-reload
 
 echo -e "\033[32mSucceeded"
