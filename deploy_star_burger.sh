c0f5a8a6653d402543d801b7635e76919a5e7c70
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-12-25 23:31:02 +0400

    Update deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -2,8 +2,26 @@
 
 git pull
 pip install -r requirements.txt
-npm ci --dev
 python3 manage.py collectstatic --noinput
-python3 manage.py migrate
+python3 manage.py migrate --noinput
+
+npm ci --dev
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+
+commit=`git rev-parse HEAD`
+
+curl --request POST \
+     --url https://api.rollbar.com/api/1/deploy \
+     --header "accept: application/json" \
+     --header "content-type: application/json" \
+     --header "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" \
+     -d '{
+  "environment": "production",
+  "revision": "'$commit'",
+  "rollbar_username": "User",
+  "local_username": "User",
+  "comment": "Deploy",
+  "status": "Succeeded"
+}'
 
 echo -e "\033[32mSucceeded"
